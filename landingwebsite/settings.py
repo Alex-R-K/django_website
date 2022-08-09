c99bcd188133aca9e67a4e1bb2347d52a9cecf90
Alex-R-K <gt450ch4@mail.ru>
2022-08-09 12:16:16 +0300

    third_commit

--- landingwebsite/settings.py
+++ landingwebsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crm.apps.CrmConfig', # add app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'landingwebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'], # 5. path to html page(landingwabpage/templates/index.html)
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- landingwebsite/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-def first_page(request):
-    a = '<h1>Hello World</h1>'
-    return HttpResponse(a)
