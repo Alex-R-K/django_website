5a664c3da2774ce2e2bd3f36b52284e1383443bc
Alex-R-K <gt450ch4@mail.ru>
2022-08-14 21:34:25 +0300

    5th_commit

--- crm/forms.py
+++ crm/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class OrderForm(forms.Form):
-    name = forms.CharField(max_length=200)  # "required=False" make form's field optional |||| wiget=forms.TextInput(
-    # attrs={'class':'css.input'}))
+    name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'css_input'}))  # "required=False" make form's field optional |||| wiget=forms.TextInput(
+    # attrs={'class':'css_input'}))
     phone = forms.CharField(max_length=200)
     # go to views.py
